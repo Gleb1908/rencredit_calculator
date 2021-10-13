c2a3af1bd9379549d68f89db1d095c6212801248
Gleb1908 <gleb1908@list.ru>
2021-10-13 15:26:13 +0300

    added PageManager.java

---
+++ src/main/java/managers/PageManager.java
@@ -0,0 +1,68 @@
+package managers;
+
+import pages.HomePage;
+import pages.CalculatorPage;
+
+public class PageManager {
+
+    /**
+     * Менеджер страничек
+     */
+    private static PageManager pageManager;
+
+    /**
+     * Стартовая страничка
+     */
+    private HomePage homePage;
+
+    /**
+     * Страничка с результатами поиска
+     */
+    private CalculatorPage calculatorPage;
+
+    /**
+     * Конструктор специально был объявлен как private (singleton паттерн)
+     *
+     * @see PageManager#getPageManager()
+     */
+    private PageManager() {
+    }
+
+    /**
+     * Ленивая инициализация PageManager
+     *
+     * @return PageManager
+     */
+    public static PageManager getPageManager() {
+        if (pageManager == null) {
+            pageManager = new PageManager();
+        }
+        return pageManager;
+    }
+
+    /**
+     * Ленивая инициализация {@link HomePage}
+     *
+     * @return HomePage
+     */
+    public HomePage getHomePage() {
+        if (homePage == null) {
+            homePage = new HomePage();
+        }
+        return homePage;
+    }
+
+    /**
+     * Ленивая инициализация {@link CalculatorPage}
+     *
+     * @return ResultsPage
+     */
+    public CalculatorPage getCalculatorPage() {
+        if (calculatorPage == null) {
+            calculatorPage = new CalculatorPage();
+        }
+        return calculatorPage;
+    }
+
+
+}
